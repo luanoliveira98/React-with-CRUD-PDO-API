d0e1cc756c2996a4d3d8706177efb690c8683704
Luan Oliveira <luanoliveira17@hotmail.com>
2021-07-09 00:51:58 -0300

    Rotas do crud de especialidades

--- crud/src/App.js
+++ crud/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import {Home} from './pages/Home';
+
 import {PacientesList} from './pages/Pacientes/List';
 import {PacientesCreate} from './pages/Pacientes/Create';
 import {PacientesShow} from './pages/Pacientes/Show';
 import {PacientesEdit} from './pages/Pacientes/Edit';
+
+import {EspecialidadesList} from './pages/Especialidades/List';
+import {EspecialidadesCreate} from './pages/Especialidades/Create';
+import {EspecialidadesEdit} from './pages/Especialidades/Edit';
 
 function App() {
   return (
@@ -13,10 +18,15 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}/>
+
           <Route exact path="/pacientes" component={PacientesList}/>
           <Route path="/pacientes/cadastrar" component={PacientesCreate}/>
           <Route exact path="/pacientes/:id" component={PacientesShow}/>
           <Route exact path="/pacientes/:id/editar" component={PacientesEdit}/>
+
+          <Route exact path="/especialidades" component={EspecialidadesList}/>
+          <Route path="/especialidades/cadastrar" component={EspecialidadesCreate}/>
+          <Route path="/especialidades/:id/editar" component={EspecialidadesEdit}/>
         </Switch>
       </Router>
     </div>
